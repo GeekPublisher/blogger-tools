e4a3c96672a5fe87b66edae402089d4081129562
GeekPublisher <GeekPublisher@users.noreply.github.com>
2016-08-05 04:51:16 +0800

    Create showRecentPosts.js

---
+++ facebook-trending/js/showRecentPosts.js
@@ -0,0 +1,26 @@
+var numposts = 10;
+var standardstyling = true;
+function showRecentPosts(json) {
+document.write('<table>');
+  for (var i = 0; i < numposts; i++) {
+    var entry = json.feed.entry[i];
+    var posttitle = entry.title.$t;
+    var posturl;
+    if (i == json.feed.entry.length) break;
+    for (var k = 0; k < entry.link.length; k++) {
+      if (entry.link[k].rel == 'alternate') {
+        posturl = entry.link[k].href;
+        break;
+      }}
+    posttitle = posttitle.link(posturl);
+    if (standardstyling) 
+    document.write('<tr><td>');
+    document.write('<span class="fb-comments-count" data-layout="button_count" data-href="'+posturl+'"></span>');
+    document.write('</td><td>');
+    document.write('<span class="fb-share-button" data-layout="button_count" data-href="'+posturl+'"></span>');
+    document.write('</td><td>');
+    document.write(posttitle);
+    document.write('</td></tr>');
+  }
+document.write('</table>');
+}
